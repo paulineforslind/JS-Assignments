279b21d472ac22d66367d37b5969b70968b5d735
paulineforslind <paufor595@ihmeducation.se>
2024-09-12 14:11:17 +0200

    Add basic operators

---
+++ P5Operators/js/app.js
@@ -0,0 +1,46 @@
+let x = 5 + 5; // x = 10
+let y = x + 5; // y = 15
+let z = y + y; // z = 30
+console.log(z + z); // 60
+let firstNumber = 7;
+let secondNumber = 2;
+let seven = 7;
+let answer = 0;
+
+// 1 + 2 = 3
+answer = firstNumber + secondNumber
+console.log(answer);
+
+answer = firstNumber - secondNumber;
+console.log(answer);
+
+answer = firstNumber * secondNumber;
+console.log(answer);
+
+answer = firstNumber / secondNumber;
+console.log(answer);
+
+answer = firstNumber % secondNumber;
+console.log(answer);
+
+let gold = 50;
+gold + 10;
+console.log(gold); // 50
+
+gold = gold + 10;
+console.log(gold); // 60
+
+gold += 10;
+console.log(gold); // 70
+
+gold *= 10;
+console.log(gold); //700
+
+gold /= 10;
+console.log(gold); // 70
+
+gold -= 10;
+console.log(gold); // 60
+
+gold %= 2;
+console.log(gold); // 0
