fbb099f7aba54200f459f74b9a323272794e4559
paulineforslind <paufor595@ihmeducation.se>
2024-09-17 09:41:06 +0200

    Added code

--- P10OutputAndInput/js/app.js
+++ P10OutputAndInput/js/app.js
@@ -1,4 +1,31 @@
 function getBankBalance() {
-  return "You're broke";
+  return 500 + 600;
 }
 console.log(getBankBalance());
+
+
+
+
+// function with input(s) and no output
+function addOne(inputNumber) {
+  inputNumber += 1;
+  console.log(inputNumber);
+}
+
+// calling the function
+addOne(8);
+
+
+
+
+// function with input(s) and an output
+function square(numberToSquare) {
+  return numberToSquare * numberToSquare;
+}
+
+// calling the function
+let squaredNumber = square(15);
+console.log(squaredNumber);
+
+// or simplified
+console.log(square(15));
