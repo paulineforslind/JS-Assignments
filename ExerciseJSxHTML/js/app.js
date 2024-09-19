c175e4526a38a8faf911ba6685e8340b4653cf8a
paulineforslind <paufor595@ihmeducation.se>
2024-09-19 10:47:31 +0200

    Refactor

--- ExerciseJSxHTML/js/app.js
+++ ExerciseJSxHTML/js/app.js
@@ -1,12 +1,38 @@
+// Document Elements
 // Looks for "text0", assigns that to the variable called "textField0"
 const textField0 = document.getElementById("text0");
-textField0.innerHTML = "something else";
-textField0.innerHTML = showStylizedScore(786);
+const textField1 = document.getElementById("text1");
 
-function showScore(inputNumber) {
+
+// Internal Variables
+let score = 0;
+// textField0.innerHTML = "something else";
+// textField0.innerHTML = showStylizedScore(786);
+
+
+// Process (What is going to happen in what sequence)
+increaseScoreByOne();
+
+
+// Controllers
+/*function showScore(inputNumber) {
   return inputNumber * 100; // Output is a number
 }
 
 function showStylizedScore(scoreInput) {
   return "Your score is: " + scoreInput * 100;
+} */
+
+function increaseScoreByOne() {
+  score++;
+  updateScoreText(score);
+}
+
+
+// View
+function updateScoreText(newText) {
+  textField0.innerHTML = newText;
+}
+function updateStatusText(newText) {
+  textField1.innerHTML = newText;
 }
